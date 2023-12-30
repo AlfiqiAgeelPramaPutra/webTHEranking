aa00764c1e09cdf041fda48827a151e4c3aefed3
Alfiqi Ageel <alfiqiageelpp1@gmail.com>
2023-12-30 18:21:58 +0700

    done

---
+++ Web/landing.php
@@ -0,0 +1,367 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" />
+  <link rel="stylesheet" href="style.css" />
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+  <title>Landing</title>
+  <style>
+        table {
+            margin-top: 20px;
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        table th,
+        table td {
+            padding: 8px;
+            text-align: left;
+            border-bottom: 1px solid yellow;
+        }
+    </style>
+</head>
+
+<body>
+  <nav class="navbar sticky-top navbar-expand-sm bg-dark navbar-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">My Logo</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="landing.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="index.html">Compare</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="about.html">About us</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <div class="container-fluid pt-3 text-center">
+    <h1>Selamat datang!</h1>
+  </div>
+  <!-- <div class="border"> -->
+  <div class="container mt-5">
+    <h3>Silahkan pilih parameter yang akan digunakan!</h3>
+    
+
+    <div class="mt-5 dropdown">
+      <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown">
+        Klik untuk melihat parameter yang tersedia
+      </button>
+      <ul class="dropdown-menu">
+        <form action="landing.php" method="POST">
+          <select name="parameter" class="dropdown-item">
+            <option value="research" name="research">Research</option>
+            <option value="citation" name="citation">Citation</option>
+            <option value="teaching" name="teaching">Teaching</option>
+            <option value="industry_income" name="industry_income">Industry Income</option>
+            <option value="international_outlook" name="international_outlook">International Outlook</option>
+            <option value="ova" name="ova">Overall Score</option>
+            <option value="campus_info" name="campus_info">Campus Info</option>
+          </select>
+          <input type="submit" value="Submit" name="submit">
+        </form>
+        <!-- <form action="landing.php">
+        <input type="text" name="university_name" placeholder="Enter university name">
+
+        </form> -->
+      </ul>
+    </div>
+
+
+  </div>
+  <!-- </div> -->
+  <?php
+    if (isset($_POST['submit'])) {
+        $selectedParameter = $_POST['parameter'];
+        echo "<h4>You selected: $selectedParameter</h4>";
+
+        // Database connection settings
+        $host = "localhost";
+        $username = "root";
+        $password = "";
+        $database = "analisis_the_baru";
+
+        // Create a database connection
+        $conn = new mysqli($host, $username, $password, $database);
+
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        // $universityName = $_POST['university_name'];
+
+        // SQL query to select data  from the respective tables based on the selected parameter
+        $sql = "";
+        if ($selectedParameter === "campus_info") {
+            $sql = "SELECT * FROM campus_info";
+            if ($sql !== "") {
+                $result = $conn->query($sql);
+
+                if ($result->num_rows > 0) {
+                    echo "<table>";
+                    echo "<tr>";
+                    echo "<th>ID</th>";
+                    echo "<th>World Rank</th>";
+                    echo "<th>Nama Universitas</th>";
+                    echo "<th>Lokasi</th>";
+                    echo "</tr>";
+
+                    // Output data of each row
+                    while ($row = $result->fetch_assoc()) {
+                        echo "<tr>";
+                        echo "<td>" . $row["id_info"] . "</td>";
+                        echo "<td>" . $row["wrld_rank"] . "</td>";
+                        echo "<td>" . $row["nama_univ"] . "</td>";
+                        echo "<td>" . $row["lokasi"] . "</td>";
+                        echo "</tr>";
+                    }
+
+                    echo "</table>";
+                } else {
+                    echo "No data available.";
+                }
+            }
+        }
+        elseif ($selectedParameter === "citation") {
+            $sql = "SELECT * FROM citation";
+            if ($sql !== "") {
+                $result = $conn->query($sql);
+    
+                if ($result->num_rows > 0) {
+                    echo "<table>";
+                    echo "<tr>";
+                    echo "<th>ID</th>";
+                    echo "<th>World Rank</th>";
+                    echo "<th>Nama Universitas</th>";
+                    echo "<th>Lokasi</th>";
+                    echo "<th>Citation</th>";
+                    echo "<th>Rank Citation</th>";
+                    echo "</tr>";
+    
+                    // Output data of each row
+                    while ($row = $result->fetch_assoc()) {
+                        echo "<tr>";
+                        echo "<td>" . $row["id_ctn"] . "</td>";
+                        echo "<td>" . $row["wrld_rank"] . "</td>";
+                        echo "<td>" . $row["nama_univ"] . "</td>";
+                        echo "<td>" . $row["lokasi"] . "</td>";
+                        echo "<td>" . $row["citation"] . "</td>";
+                        echo "<td>" . $row["rank_ctn"] . "</td>";
+                        echo "</tr>";
+                    }
+    
+                    echo "</table>";
+                } else {
+                    echo "No data available.";
+                }
+            }
+        }
+        elseif ($selectedParameter === "research") {
+            $sql = "SELECT * FROM research";
+            if ($sql !== "") {
+                $result = $conn->query($sql);
+    
+                if ($result->num_rows > 0) {
+                    echo "<table>";
+                    echo "<tr>";
+                    echo "<th>ID</th>";
+                    echo "<th>World Rank</th>";
+                    echo "<th>Nama Universitas</th>";
+                    echo "<th>Lokasi</th>";
+                    echo "<th>Research</th>";
+                    echo "<th>Rank Research</th>";
+                    echo "</tr>";
+    
+                    // Output data of each row
+                    while ($row = $result->fetch_assoc()) {
+                        echo "<tr>";
+                        echo "<td>" . $row["id_rsc"] . "</td>";
+                        echo "<td>" . $row["wrld_rank"] . "</td>";
+                        echo "<td>" . $row["nama_univ"] . "</td>";
+                        echo "<td>" . $row["lokasi"] . "</td>";
+                        echo "<td>" . $row["research"] . "</td>";
+                        echo "<td>" . $row["rank_rsc"] . "</td>";
+                        echo "</tr>";
+                    }
+    
+                    echo "</table>";
+                } else {
+                    echo "No data available.";
+                }
+            }
+        } 
+        elseif ($selectedParameter === "industry_income") {
+            $sql = "SELECT * FROM income";
+            if ($sql !== "") {
+                $result = $conn->query($sql);
+    
+                if ($result->num_rows > 0) {
+                    echo "<table>";
+                    echo "<tr>";
+                    echo "<th>ID</th>";
+                    echo "<th>World Rank</th>";
+                    echo "<th>Nama Universitas</th>";
+                    echo "<th>Lokasi</th>";
+                    echo "<th>Industry Income</th>";
+                    echo "<th>Rank Industry Income</th>";
+                    echo "</tr>";
+    
+                    // Output data of each row
+                    while ($row = $result->fetch_assoc()) {
+                        echo "<tr>";
+                        echo "<td>" . $row["id_inc"] . "</td>";
+                        echo "<td>" . $row["wrld_rank"] . "</td>";
+                        echo "<td>" . $row["nama_univ"] . "</td>";
+                        echo "<td>" . $row["lokasi"] . "</td>";
+                        echo "<td>" . $row["income"] . "</td>";
+                        echo "<td>" . $row["rank_inc"] . "</td>";
+                        echo "</tr>";
+                    }
+    
+                    echo "</table>";
+                } else {
+                    echo "No data available.";
+                }
+            }
+        } 
+        elseif ($selectedParameter === "international_outlook") {
+            $sql = "SELECT * FROM int_outlook";
+            if ($sql !== "") {
+                $result = $conn->query($sql);
+    
+                if ($result->num_rows > 0) {
+                    echo "<table>";
+                    echo "<tr>";
+                    echo "<th>ID</th>";
+                    echo "<th>World Rank</th>";
+                    echo "<th>Nama Universitas</th>";
+                    echo "<th>Lokasi</th>";
+                    echo "<th>International Outlook</th>";
+                    echo "<th>Rank International Outlokk</th>";
+                    echo "</tr>";
+    
+                    // Output data of each row
+                    while ($row = $result->fetch_assoc()) {
+                        echo "<tr>";
+                        echo "<td>" . $row["id_int_outlook"] . "</td>";
+                        echo "<td>" . $row["wrld_rank"] . "</td>";
+                        echo "<td>" . $row["nama_univ"] . "</td>";
+                        echo "<td>" . $row["lokasi"] . "</td>";
+                        echo "<td>" . $row["int_outlook"] . "</td>";
+                        echo "<td>" . $row["rank_int_outlook"] . "</td>";
+                        echo "</tr>";
+                    }
+    
+                    echo "</table>";
+                } else {
+                    echo "No data available.";
+                }
+            }
+        } 
+        elseif ($selectedParameter === "teaching") {
+            $sql = "SELECT * FROM teaching";
+            if ($sql !== "") {
+                $result = $conn->query($sql);
+    
+                if ($result->num_rows > 0) {
+                    echo "<table>";
+                    echo "<tr>";
+                    echo "<th>ID</th>";
+                    echo "<th>World Rank</th>";
+                    echo "<th>Nama Universitas</th>";
+                    echo "<th>Lokasi</th>";
+                    echo "<th>Teaching</th>";
+                    echo "<th>Rank Teaching</th>";
+                    echo "</tr>";
+    
+                    // Output data of each row
+                    while ($row = $result->fetch_assoc()) {
+                        echo "<tr>";
+                        echo "<td>" . $row["id_teaching"] . "</td>";
+                        echo "<td>" . $row["wrld_rank"] . "</td>";
+                        echo "<td>" . $row["nama_univ"] . "</td>";
+                        echo "<td>" . $row["lokasi"] . "</td>";
+                        echo "<td>" . $row["teaching"] . "</td>";
+                        echo "<td>" . $row["rank_teaching"] . "</td>";
+                        echo "</tr>";
+                    }
+    
+                    echo "</table>";
+                } else {
+                    echo "No data available.";
+                } 
+            }
+        } 
+        elseif ($selectedParameter === "ova") {
+            $sql = "SELECT * FROM overall";
+            if ($sql !== "") {//
+                $result = $conn->query($sql);
+    
+                if ($result->num_rows > 0) {
+                    echo "<table>";
+                    echo "<tr>";
+                    echo "<th>ID</th>";
+                    echo "<th>World Rank</th>";
+                    echo "<th>Nama Universitas</th>";
+                    echo "<th>Lokasi</th>";
+                    echo "<th>Citation</th>";
+                    echo "<th>Rank Citation</th>";
+                    echo "<th>Teaching</th>";
+                    echo "<th>Rank Teaching</th>";
+                    echo "<th>International Outlook</th>";
+                    echo "<th>Rank International Outlokk</th>";
+                    echo "<th>Industry Income</th>";
+                    echo "<th>Rank Industry Income</th>";
+                    echo "<th>Research</th>";
+                    echo "<th>Rank Research</th>";
+                    echo "<th>Citation</th>";
+                    echo "<th>Rank Citation</th>";
+                    echo "</tr>";
+    
+                    // Output data of each row
+                    while ($row = $result->fetch_assoc()) {
+                        echo "<tr>";
+                        echo "<td>" . $row["id_ova"] . "</td>";
+                        echo "<td>" . $row["wrld_rank"] . "</td>";
+                        echo "<td>" . $row["nama_univ"] . "</td>";
+                        echo "<td>" . $row["lokasi"] . "</td>";
+                        echo "<td>" . $row["citation"] . "</td>";
+                        echo "<td>" . $row["rank_ctn"] . "</td>";
+                        echo "<td>" . $row["teaching"] . "</td>";
+                        echo "<td>" . $row["rank_teaching"] . "</td>";
+                        echo "<td>" . $row["int_outlook"] . "</td>";
+                        echo "<td>" . $row["rank_int_outlook"] . "</td>";
+                        echo "<td>" . $row["income"] . "</td>";
+                        echo "<td>" . $row["rank_inc"] . "</td>";
+                        echo "<td>" . $row["research"] . "</td>";
+                        echo "<td>" . $row["rank_rsc"] . "</td>";
+                        echo "<td>" . $row["citation"] . "</td>";
+                        echo "<td>" . $row["rank_ctn"] . "</td>";
+                        echo "</tr>";
+                    }
+    
+                    echo "</table>";
+                } else {
+                    echo "No data available.";
+                }
+            }
+        } 
+        $conn->close();
+    }
+    ?>
+
+</body>
+
+</html>
---
+++ Web/src/main.js
@@ -0,0 +1,313 @@
+for (var i = 1; i < universitas.length; i++) {
+    document.getElementById("select1").innerHTML += `
+    <option value="${i}">${universitas[i].nama_univ}</option>
+    `;
+    document.getElementById("select2").innerHTML += `
+    <option value="${i}">${universitas[i].nama_univ}</option>
+    `;
+    document.getElementById("select3").innerHTML += `
+    <option value="${i}">${universitas[i].nama_univ}</option>
+    `;
+}
+
+function item1(a) {
+    var select1 = document.getElementById("select1").value;
+    if (a == select1) {
+        document.getElementById("univ1").innerHTML = universitas[a].nama_univ
+        document.getElementById("lokasi1").innerHTML = universitas[a].lokasi
+        document.getElementById("wrld_rank1").innerHTML = universitas[a].wrld_rank
+        document.getElementById("teaching1").innerHTML = universitas[a].teaching
+        document.getElementById("research1").innerHTML = universitas[a].research
+        document.getElementById("citation1").innerHTML = universitas[a].citation
+        document.getElementById("outlook1").innerHTML = universitas[a].int_outlook
+        //hapus di bawah bila gagal
+        // document.getElementById("point1").innerHTML = universitas[a].pointsNeeded
+        var parameter = "teaching";
+        var currentPoints = teaching1;
+        var result = calculatePointsToNextRank(parameter, currentPoints);
+        console.log(result);
+    
+
+    }
+ 
+}
+
+function item2(a) {
+    var select2 = document.getElementById("select2").value;
+    if (a == select2) {
+        document.getElementById("univ2").innerHTML = universitas[a].nama_univ
+        document.getElementById("lokasi2").innerHTML = universitas[a].lokasi
+        document.getElementById("wrld_rank2").innerHTML = universitas[a].wrld_rank
+        document.getElementById("teaching2").innerHTML = universitas[a].teaching
+        document.getElementById("research2").innerHTML = universitas[a].research
+        document.getElementById("citation2").innerHTML = universitas[a].citation
+        document.getElementById("outlook2").innerHTML = universitas[a].int_outlook
+        
+    } else {
+        document.getElementById("select3").selectedIndex = 0;
+        document.getElementById("img3").src = universitas[0].image
+        document.getElementById("price3").innerHTML = ""
+        document.getElementById("desc3").innerHTML = ''
+        document.getElementById("brand3").innerHTML = ""
+
+    }
+}
+
+function item3(a) {
+    var select3 = document.getElementById("select3").value;
+    if (a == select3) {
+        document.getElementById("univ3").innerHTML = universitas[a].nama_univ
+        document.getElementById("lokasi3").innerHTML = universitas[a].lokasi
+        document.getElementById("wrld_rank3").innerHTML = universitas[a].wrld_rank
+        document.getElementById("teaching3").innerHTML = universitas[a].teaching
+        document.getElementById("research3").innerHTML = universitas[a].research
+        document.getElementById("citation3").innerHTML = universitas[a].citation
+        document.getElementById("outlook3").innerHTML = universitas[a].int_outlook
+    } else {
+        document.getElementById("select3").selectedIndex = 0;
+        document.getElementById("img3").src = universitas[0].image
+        document.getElementById("price3").innerHTML = ""
+        document.getElementById("desc3").innerHTML = ''
+        document.getElementById("brand3").innerHTML = ""
+
+    }
+}
+// ini juga
+function calculatePointsToNextRank(parameter, currentPoints) {
+    var nextRankPoints = parseInt(universitas[1][parameter]);
+    var pointsNeeded = nextRankPoints - currentPoints;
+  
+    if (pointsNeeded <= 0) {
+      return "Congratulations! You have already reached the next rank in this parameter.";
+    } else {
+      return "You need " + pointsNeeded + " more points to reach the next rank in this parameter.";
+    }
+}
+
+// Array to store parameter values
+var teachingValues = [];
+var researchValues = [];
+var citationValues = [];
+var outlookValues = [];
+
+// Loop through the universitas array and extract parameter values
+for (var i = 1; i < universitas.length; i++) {
+  teachingValues.push(universitas[i].teaching);
+  researchValues.push(universitas[i].research);
+  citationValues.push(universitas[i].citation);
+  outlookValues.push(universitas[i].int_outlook);
+}
+// /////////////////////////////////////
+var ctx = document.getElementById('combinedChart').getContext('2d');
+new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: teachingValues.map((_, i) => `University ${i + 1}`),
+    datasets: [
+      {
+        label: 'Teaching',
+        data: teachingValues,
+        backgroundColor: 'rgba(75, 192, 192, 0.6)',
+        borderColor: 'rgba(75, 192, 192, 1)',
+        borderWidth: 1
+      },
+      {
+        label: 'Outlook',
+        data: outlookValues,
+        backgroundColor: 'rgba(255, 99, 132, 0.6)',
+        borderColor: 'rgba(255, 99, 132, 1)',
+        borderWidth: 1
+      },
+      {
+        label: 'Research',
+        data: researchValues,
+        backgroundColor: 'rgba(54, 162, 235, 0.6)',
+        borderColor: 'rgba(54, 162, 235, 1)',
+        borderWidth: 1
+      },
+      {
+        label: 'Citations',
+        data: citationValues,
+        backgroundColor: 'rgba(54, 162, 12, 0.6)',
+        borderColor: 'rgba(54, 162, 12, 1)',
+        borderWidth: 1
+      },
+      // Add more datasets for other parameters
+    ]
+  },
+  options: {
+    responsive: true,
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
+
+// Add line chart
+var lineChartCtx = document.getElementById('lineChart').getContext('2d');
+new Chart(lineChartCtx, {
+  type: 'line',
+  data: {
+    labels: teachingValues.map((_, i) => `University ${i + 1}`),
+    datasets: [
+      {
+        label: 'Teaching',
+        data: teachingValues,
+        borderColor: 'rgba(75, 192, 192, 1)',
+        backgroundColor: 'rgba(0, 0, 0, 0)',
+        borderWidth: 1
+      },
+      // Add more datasets for other parameters
+    ]
+  },
+  options: {
+    responsive: true,
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
+
+window.onload = function () {
+  var chart = new CanvasJS.Chart("chartContainer",
+  {
+    title:{
+      text: "A Combination of Column & Line Chart"
+    
+    },   
+    data: [{        
+      type: "column",
+      dataPoints: [
+      { x: teachingValues, y: 171 },
+      { x: 20, y: 155},
+      { x: 30, y: 150 },
+      { x: 40, y: 165 },
+      { x: 50, y: 195 },
+      { x: 60, y: 168 },
+      { x: 70, y: 128 },
+      { x: 80, y: 134 },
+      { x: 90, y: 114}
+      ]
+    },
+    {        
+      type: "line",
+      dataPoints: [
+      { x: 10, y: 71 },
+      { x: 20, y: 55},
+      { x: 30, y: 50 },
+      { x: 40, y: 65 },
+      { x: 50, y: 95 },
+      { x: 60, y: 68 },
+      { x: 70, y: 28 },
+      { x: 80, y: 34 },
+      { x: 90, y: 14}
+      ]
+    }
+      
+    ]
+  });
+
+  chart.render();
+}
+/////////////////////////////////////////
+// Create a bar chart for Teaching parameter
+var ctxTeaching = document.getElementById('chartTeaching').getContext('2d');
+new Chart(ctxTeaching, {
+  type: 'bar',
+  data: {
+    labels: teachingValues.map((_, i) => `University ${i + 1}`),
+    datasets: [{
+      label: 'Teaching',
+      data: teachingValues,
+      backgroundColor: 'rgba(75, 192, 192, 0.6)',
+      borderColor: 'rgba(75, 192, 192, 1)',
+      borderWidth: 1
+    }]
+  },
+  options: {
+    responsive: true,
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
+
+// Create a bar chart for Research parameter
+var ctxResearch = document.getElementById('chartResearch').getContext('2d');
+new Chart(ctxResearch, {
+  type: 'bar',
+  data: {
+    labels: researchValues.map((_, i) => `University ${i + 1}`),
+    datasets: [{
+      label: 'Research',
+      data: researchValues,
+      backgroundColor: 'rgba(255, 99, 132, 0.6)',
+      borderColor: 'rgba(255, 99, 132, 1)',
+      borderWidth: 1
+    }]
+  },
+  options: {
+    responsive: true,
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
+
+// Create a bar chart for Citation parameter
+var ctxCitation = document.getElementById('chartCitation').getContext('2d');
+new Chart(ctxCitation, {
+  type: 'bar',
+  data: {
+    labels: citationValues.map((_, i) => `University ${i + 1}`),
+    datasets: [{
+      label: 'Citation',
+      data: citationValues,
+      backgroundColor: 'rgba(54, 162, 235, 0.6)',
+      borderColor: 'rgba(54, 162, 235, 1)',
+      borderWidth: 1
+    }]
+  },
+  options: {
+    responsive: true,
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
+
+// Create a bar chart for Outlook parameter
+var ctxOutlook = document.getElementById('chartOutlook').getContext('2d');
+new Chart(ctxOutlook, {
+  type: 'bar',
+  data: {
+    labels: outlookValues.map((_, i) => `University ${i + 1}`),
+    datasets: [{
+      label: 'Outlook',
+      data: outlookValues,
+      backgroundColor: 'rgba(153, 102, 255, 0.6)',
+      borderColor: 'rgba(153, 102, 255, 1)',
+      borderWidth: 1
+    }]
+  },
+  options: {
+    responsive: true,
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
+
+  
